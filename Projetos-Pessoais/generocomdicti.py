82ad45cf77a7bf7d7ec9c52b1119ba0ea6537ecc
brunonativaa <brunoferreirarodrigues26@outlook.com>
2026-03-14 15:44:26 -0300

    Enviando o scrip automoção de pasta. e modificando projetos pessoais

--- Projetos-Pessoais/generocomdicti.py
+++ Projetos-Pessoais/generocomdicti.py
@@ -1,10 +1,10 @@
 opcao = {
     '1': 'Você é Mulher.',
     '2': 'Você é Homem.',
-    '3': 'Você é outro',
+   
 }
 
-sexo = input('Escolha um genero.\n[1] Mulher\n[2] Homem\n[3] Outro \n ')
+sexo = input('Escolha um genero.\n[1] Mulher\n[2] Homem\n ')
 
 resultados = opcao.get(sexo, 'Opção invalida')
 print(resultados)
--- Projetos-Pessoais/nota.py
+++ Projetos-Pessoais/nota.py
@@ -2,4 +2,4 @@ nota = float(input("Digite uma nota entre 0 e 10: "))
 while (nota < 0) or (nota > 10):
     nota = float(input("Não pode ser menor que 0 e maior que 10 my friend! \n \
                        Tente novamente: "))
-print('Nota valida!')
+print('Nota valida!!!')
