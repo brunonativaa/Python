a98b010d9d94d75ed66c32758a1755654010b624
brunonativaa <brunoferreirarodrigues26@outlook.com>
2026-03-27 16:33:22 -0300

    Modificando listagem7.9

--- Introdução a programação com python/Trabalhando com Strings/7.3 Pesquisa de string/listagem7.9.py
+++ Introdução a programação com python/Trabalhando com Strings/7.3 Pesquisa de string/listagem7.9.py
@@ -2,7 +2,7 @@
 você pode utilizar o método find  """
 
 s = "Alô mundo"
-print(s.find('mun'))
+print(s.find('mund'))
 print(s.find('ok'))
 
 # Para pesquisar se uma string está dentro de outra e obter a posição da primeira ocorrência, você pode utilizar o método find
