40cf91a4d0dc3a9d69efc4c02a5bfca7f4902ef6
brunonativaa <brunoferreirarodrigues26@outlook.com>
2026-03-31 15:37:17 -0300

    enviando scripts de lista

---
+++ Introdução a programação com python/01 - Estrutura de dados/01 - Listas/11_extend.py
@@ -0,0 +1,7 @@
+linguagens = ["python", "js", "c"]
+
+print(linguagens)  # ["python", "js", "c"]
+
+linguagens.extend(["java", "csharp"]) # usando o método extend() para adicionar os elementos "java" e "csharp" à lista de linguagens
+
+print(linguagens)  # ["python", "js", "c", "java", "csharp"]
---
+++ Introdução a programação com python/01 - Estrutura de dados/01 - Listas/17_len.py
@@ -0,0 +1,3 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(len(linguagens))  # Printando o comprimento da lista de linguagens, que é 5
